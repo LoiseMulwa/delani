28b4b6493f7ca7b2e78a2298775c8cf6aec5aead
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-13 16:29:03 +0300

    ft: Added a toggle method to show the description when image is clicked

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#serv").click(function(event){
+        $("#des").toggle();
+        $("#hide").toggle();
+
+    })
+})
+
+$(document).ready(function(){
+    $("#dev").click(function(event){
+        $("#loy").toggle();
+        $("#hi").toggle();
+
+    })
+})
+
+$(document).ready(function(){
+    $("#pro").click(function(event){
+        $("#product").toggle();
+        $("#plan").toggle();
+
+    })
+})
+
