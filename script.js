f1c7ac6f5d8bf7a3ee1c6c29725b07b5f6e8c184
LoiseMulwa <loisemulwa7@gmail.com>
2022-03-14 02:36:03 +0300

    ft: Added a popup message for the form submission

--- script.js
+++ script.js
@@ -22,3 +22,18 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function(){
+    $("#num1").mouseover(function(){
+      $("num11").show();
+    }).mouseout(function(){
+      $("#num11").hide();
+    });
+  });
+let myForm=document.querySelector("#mail")
+  myForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let jina = myForm.jina.value;
+    alert(jina + "" + " we have received your message.Thank you for reaching out to us." );
+})
+
+  
